b9e8a51f947c81c62f22914aed132767b3c530eb
lamdarv <lamda.richo.tif420@polban.ac.id>
2022-12-06 18:15:19 +0700

    upload product with browse
    gapake product controller, disatuin di product route

---
+++ models/MemberModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Member = mongoose.Schema({
+  member_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+export default mongoose.model("Member", Member);
